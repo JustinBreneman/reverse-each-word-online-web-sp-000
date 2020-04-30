5c6d5d436dbf9265fb960c3c20cf5560837a879d
Justin Breneman <justin.breneman@yahoo.com>
2020-04-30 23:33:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  reversed = array.collect do |word|
+  array.collect do |word|
     word.reverse
-  end
-  reversed.join(" ")
+  end.join(" ")
 end
